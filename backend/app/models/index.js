01b14691a8be3a3495bde61b7e035b710187835b
evonchong <evon.chong@11-technology.io>
2023-10-06 16:10:55 +0800

    Backend - update database config

---
+++ backend/app/models/index.js
@@ -0,0 +1,16 @@
+// retrieve db settings from config
+const dbConfig = require("../config/db");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+module.exports = db;
